72253efbd697df67ebe167cbd895fc2fd64f0dcc
frankie02m2023 <your.email@example.com>
2024-05-07 20:51:13 +0100

    Toy and medium dataset loading apparently working.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 //
 // Created by franc on 07/05/2024.
 //
+
+#include "TSPAlgorithms.h"
  int main(){
+    TSPAlgorithms tspAlgorithm = TSPAlgorithms("edges_500");
+    tspAlgorithm.loadGraph();
+    for(Vertex<Node>* vertex : tspAlgorithm.getGraph().getVertexSet()){
+        for(Edge<Node>* edge : vertex->getAdj()){
+            cout << vertex->getInfo().getID() << " -> " << edge->getDest()->getInfo().getID() << "  " << edge->getWeight() << endl;
+        }
+    }
     return 0;
 }
