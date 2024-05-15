ca722eae9aed34bfe29dc4d42b200b0f49cab005
bialachado <blachadop@gmail.com>
2024-05-15 13:00:38 +0100

    Implemented Menu

--- main.cpp
+++ main.cpp
@@ -2,20 +2,10 @@
 // Created by franc on 07/05/2024.
 //
 
-#include "TSPAlgorithms.h"
+#include "Menu.h"
+
  int main(){
-    TSPAlgorithms tspAlgorithm = TSPAlgorithms("edges_100");
-    tspAlgorithm.loadGraph();
-    cout << tspAlgorithm.getGraph().getVertexSet().size() << endl;
-    cout << tspAlgorithm.isGraphFullyConnected() << endl;
-    stack<Node> minPathStack;
-    vector<Node> minPathVector;
-    double minPathDistance1 = tspAlgorithm.getMinDistWithTriangularInequity(minPathVector);
-    for(Node node : minPathVector){
-        cout << node.getID() << " -> ";
-    }
-    cout << endl;
-    cout << minPathDistance1 << endl;
-    cout << endl;
-    return 0;
+     Menu menu = Menu();
+     menu.graphMenu();
+     return 0;
 }
