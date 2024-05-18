1fa8e3c35c2d2d324c13673a0f266a09a60a6f1d
frankie02m2023 <gustavonelson098@gmail.com>
2024-05-18 19:41:08 +0100

    Optimizing data parsing.

---
+++ CustomGraph.h
@@ -0,0 +1,176 @@
+//
+// Created by franc on 18/05/2024.
+//
+
+#ifndef DA2324_P03_STUDENT_CUSTOMGRAPH_H
+#define DA2324_P03_STUDENT_CUSTOMGRAPH_H
+
+#include <vector>
+#include <unordered_set>
+#include <queue>
+#include "Node.h"
+
+class Edge; // Forward declaration of Edge
+
+/********************** Vertex  ****************************/
+
+class Vertex {
+public:
+    Vertex(Node info);
+
+    Node getInfo() const;
+    std::unordered_set<Edge*, std::hash<Edge*>, std::equal_to<Edge*>> getAdj() const;
+    bool isVisited() const;
+    bool isProcessing() const;
+    unsigned int getIndegree() const;
+    double getDist() const;
+    Edge* getPath() const;
+    std::unordered_set<Edge*, std::hash<Edge*>, std::equal_to<Edge*>> getIncoming() const;
+
+    void setInfo(Node info);
+    void setVisited(bool visited);
+    void setProcessing(bool processing);
+    void setIndegree(unsigned int indegree);
+    void setDist(double dist);
+    void setPath(Edge* path);
+    Edge* addEdge(Vertex* dest, double w);
+    bool removeEdge(Node in);
+    void removeOutgoingEdges();
+    bool operator==(const Vertex& vertex) const;
+    size_t hash() const;
+
+protected:
+    Node info; // info node
+    std::unordered_set<Edge*, std::hash<Edge*>, std::equal_to<Edge*>> adj; // outgoing edges
+
+    // auxiliary fields
+    bool visited = false; // used by DFS, BFS, Prim ...
+    bool processing = false; // used by isDAG (in addition to the visited attribute)
+    unsigned int indegree; // used by topsort
+    double dist = 0;
+    Edge* path = nullptr;
+
+    std::unordered_set<Edge*, std::hash<Edge*>, std::equal_to<Edge*>> incoming; // incoming edges
+    void deleteEdge(Edge* edge);
+};
+
+struct hashVertex {
+    size_t operator()(const Vertex* vertex) const {
+        return vertex->hash();
+    }
+};
+
+struct vertexEquality {
+    bool operator()(const Vertex* v1, const Vertex* v2) const {
+        return *v1 == *v2;
+    }
+};
+
+/********************** Edge  ****************************/
+
+class Edge {
+public:
+    Edge(Vertex* orig, Vertex* dest, double w);
+
+    Vertex* getDest() const;
+    double getWeight() const;
+    bool isSelected() const;
+    Vertex* getOrig() const;
+    Edge* getReverse() const;
+    double getFlow() const;
+    bool isTraversed() const;
+
+    void setSelected(bool selected);
+    void setReverse(Edge* reverse);
+    void setFlow(double flow);
+    void setWeight(double weight);
+    void setTraversed(bool traversed);
+    bool operator==(const Edge& edge) const;
+    size_t hash() const;
+
+protected:
+    Vertex* dest; // destination vertex
+    double weight; // edge weight, can also be used for capacity
+
+    // auxiliary fields
+    bool selected = false;
+
+    // used for bidirectional edges
+    Vertex* orig;
+    Edge* reverse = nullptr;
+
+    double flow; // for flow-related problems
+
+    // used for Christofides algorithm
+    bool traversed = false;
+};
+
+namespace std {
+    template <>
+    struct hash<Edge> {
+        size_t operator()(const Edge* edge) const {
+            return edge->hash();
+        }
+    };
+
+    template <>
+    struct equal_to<Edge> {
+        bool operator()(const Edge* e1, const Edge* e2) const {
+            return *e1 == *e2;
+        }
+    };
+}
+
+/********************** Graph  ****************************/
+
+class Graph {
+public:
+    ~Graph();
+    /*
+    * Auxiliary function to find a vertex with a given the content.
+    */
+    Vertex* findVertex(const Node& in) const;
+    /*
+     *  Adds a vertex with a given content or info (in) to a graph (this).
+     *  Returns true if successful, and false if a vertex with that content already exists.
+     */
+    bool addVertex(const Node& in);
+    bool removeVertex(const Node& in);
+
+    /*
+     * Adds an edge to a graph (this), given the contents of the source and
+     * destination vertices and the edge weight (w).
+     * Returns true if successful, and false if the source or destination vertex does not exist.
+     */
+    bool addEdge(const Node& sourc, const Node& dest, double w);
+    bool removeEdge(const Node& source, const Node& dest);
+    bool addBidirectionalEdge(const Node& sourc, const Node& dest, double w);
+
+    int getNumVertex() const;
+    std::unordered_set<Vertex*, hashVertex, vertexEquality> getVertexSet() const;
+
+    std::vector<Node> dfs() const;
+    std::vector<Node> dfs(const Node& source) const;
+    void dfsVisit(Vertex* v, std::vector<Node>& res) const;
+    std::vector<Node> bfs(const Node& source) const;
+
+    bool isDAG() const;
+    bool dfsIsDAG(Vertex* v) const;
+    std::vector<Node> topsort() const;
+
+protected:
+    std::unordered_set<Vertex*, hashVertex, vertexEquality> vertexSet; // vertex set
+
+    double** distMatrix = nullptr; // dist matrix for Floyd-Warshall
+    int** pathMatrix = nullptr; // path matrix for Floyd-Warshall
+
+    /*
+     * Finds the index of the vertex with a given content.
+     */
+    int findVertexIdx(const Node& in) const;
+};
+
+
+
+#endif // DA2324_P03_STUDENT_CUSTOMGRAPH_H
+
--- TSPAlgorithms.h
+++ TSPAlgorithms.h
@@ -2,13 +2,12 @@
 // Created by franc on 07/05/2024.
 //
 
-#include "graph.h"
-#include "Node.h"
+#include "CustomGraph.h"
 #include <stack>
 
 class TSPAlgorithms{
 private:
-    Graph<Node> graph;
+    Graph graph;
     string graphName;
     bool graphIsFullyConnected;
     void loadGraphVertexes();
@@ -16,27 +15,27 @@ private:
     void setAllVertexesAsUnvisited();
     void setAllVertexPathsNull();
     void resetGraph();
-    void DFSBacktracking(Vertex<Node>* vertex, double& distance, double &minDistance, stack<Node>& path, stack<Node>& minDistPath);
-    Vertex<Node>* findMinDistVertex(vector<Vertex<Node>*> vertexes);
-    void primAlgorithm(Vertex<Node>* root);
-    void MSTPreOrderVisitDFS(Vertex<Node>* root, vector<Vertex<Node>*>& minDistancePath);
-    double getNearestNeighbourDist(Vertex<Node>* vertex);
-    double findOrCalculateDistanceBetweenVertexes(Vertex<Node>* v1, Vertex<Node>* v2);
-    void primAlgorithmChristofides(Graph<Node>& mstGraph, Vertex<Node>* vertex);
-    vector<Vertex<Node>*> getOddDegrees(Graph<Node> mstGraph);
-    bool edgeBetweenVertexes(Vertex<Node>* v1, Vertex<Node>* v2);
-    void findPerfectMatching(Graph<Node>& mstGraph,vector<Vertex<Node>*> vertex);
-    void setReverseEdgeAsTraversed(Edge<Node>* edge);
-    void setReverseEdgeAsUnTraversed(Edge<Node>* edge);
-    bool edgeIsBridge(Edge<Node>* edge, Graph<Node> &mstGraph);
-    void dfsEdgeIsBridge(Vertex<Node>* vertex, unsigned int& numberOfReachableEdges);
-    void eulerPathDFS(Vertex<Node>* vertex, vector<Vertex<Node>*>& eulerPath, Graph<Node> &mstGraph);
-    vector<Vertex<Node>*> findEulerPath(Graph<Node>& mstGraph);
-    vector<Vertex<Node>*> buildHamiltonianPath(vector<Vertex<Node>*> eulerPath);
+    void DFSBacktracking(Vertex* vertex, double& distance, double &minDistance, stack<Node>& path, stack<Node>& minDistPath);
+    Vertex* findMinDistVertex(vector<Vertex*> vertexes);
+    void primAlgorithm(Vertex* root);
+    void MSTPreOrderVisitDFS(Vertex* root, vector<Vertex*>& minDistancePath);
+    double getNearestNeighbourDist(Vertex* vertex);
+    double findOrCalculateDistanceBetweenVertexes(Vertex* v1, Vertex* v2);
+    void primAlgorithmChristofides(Graph& mstGraph, Vertex* vertex);
+    vector<Vertex*> getOddDegrees(Graph mstGraph);
+    bool edgeBetweenVertexes(Vertex* v1, Vertex* v2);
+    void findPerfectMatching(Graph& mstGraph,vector<Vertex*> vertex);
+    void setReverseEdgeAsTraversed(Edge* edge);
+    void setReverseEdgeAsUnTraversed(Edge* edge);
+    bool edgeIsBridge(Edge* edge, Graph &mstGraph);
+    void dfsEdgeIsBridge(Vertex* vertex, unsigned int& numberOfReachableEdges);
+    void eulerPathDFS(Vertex* vertex, vector<Vertex*>& eulerPath, Graph &mstGraph);
+    vector<Vertex*> findEulerPath(Graph& mstGraph);
+    vector<Vertex*> buildHamiltonianPath(vector<Vertex*> eulerPath);
 public:
     TSPAlgorithms(const string& graphName);
     string getGraphName() const;
-    Graph<Node> getGraph() const;
+    Graph getGraph() const;
     bool isGraphFullyConnected() const;
     void setGraphName(const string& graphName);
     void loadGraph();
